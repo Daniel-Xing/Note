f79c5528876b32e70324f4362cdccde90ecefec7
xz1220 <3024979463@qq.com>
2020-05-27 21:32:59 +0800

    update

--- make-dir.sh
+++ make-dir.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-mkdir -p /home/kiritoghy/labelprojectdata/image/
-mkdir -p /home/kiritoghy/labelprojectdata/images/
-mkdir -p /home/kiritoghy/labelprojectdata/imagel/
-mkdir -p /home/kiritoghy/labelprojectdata/imaged/
-mkdir -p /home/kiritoghy/labelprojectdata/video/
-mkdir -p /home/kiritoghy/labelprojectdata/videod/
-mkdir -p /home/kiritoghy/labelprojectdata/videos/
+mkdir -p ~/kiritoghy/labelprojectdata/image/
+mkdir -p ~/kiritoghy/labelprojectdata/images/
+mkdir -p ~/kiritoghy/labelprojectdata/imagel/
+mkdir -p ~/kiritoghy/labelprojectdata/imaged/
+mkdir -p ~/kiritoghy/labelprojectdata/video/
+mkdir -p ~/kiritoghy/labelprojectdata/videod/
+mkdir -p ~/kiritoghy/labelprojectdata/videos/
 
