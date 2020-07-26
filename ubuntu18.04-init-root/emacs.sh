e8e19295aa48ef2d3b4600b93ffb6fe754a744c3
xz1220 <3024979463@qq.com>
2020-07-26 23:32:58 +0800

    fix bugs and replace the ssr with trojan

--- ubuntu18.04-init-root/emacs.sh
+++ ubuntu18.04-init-root/emacs.sh
@@ -2,6 +2,6 @@
 
 sudo add-apt-repository ppa:kelleyk/emacs
 sudo apt update
-sudo apt install emacs26-nox
+sudo apt install emacs26-nox -y
 # if remove : sudo apt remove --autoremove emacs26 emacs26-nox
 cp ./.emacs.d ~/
