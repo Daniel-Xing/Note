553a0d1828b882777b686c0bc7c163f3e5a93afe
xingzheng.daniel <xingzheng.daniel@bytedance.com>
2023-06-20 20:55:59 +0800

    feature: 更新新系统初始化方法

--- ubuntu18.04-init-root/init.sh
+++ ubuntu18.04-init-root/init.sh
@@ -1,53 +1,82 @@
 #!/bin/bash
 
+# Update and upgrade the system
 sudo apt-get update -y
 sudo apt-get upgrade -y
 
-# install zsh
-sudo apt-get install zsh -y
-# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# Install zsh if not present
+if ! command -v zsh &> /dev/null; then
+    echo "Installing Zsh"
+    if ! sudo apt-get install zsh -y; then
+        echo "Failed to install zsh, cleaning up..."
+        # Add your cleanup commands for zsh here.
+    else
+        if ! sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"; then
+            echo "Failed to install oh-my-zsh, cleaning up..."
+            # Add your cleanup commands for oh-my-zsh here.
+        fi
+    fi
+fi
 
-# install byobu
-echo "Installing Byobu"
-sudo apt-get install byobu -y
+# Install Byobu if not present
+if ! command -v byobu &> /dev/null; then
+    echo "Installing Byobu"
+    if ! sudo apt-get install byobu -y; then
+        echo "Failed to install Byobu, cleaning up..."
+        # Add your cleanup commands for Byobu here.
+    fi
+fi
 
-# install git
-echo "Installing git"
-sudo apt-get install git -y
+# Install Git if not present
+if ! command -v git &> /dev/null; then
+    echo "Installing Git"
+    if ! sudo apt-get install git -y; then
+        echo "Failed to install Git, cleaning up..."
+        # Add your cleanup commands for Git here.
+    fi
+fi
 
-# install docker
-echo "Install docker"
-sudo apt-get remove docker docker-engine docker.io containerd runc -y
-sudo apt-get update -y
-sudo apt-get install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo \
-"deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+# Install Docker if not present
+if ! command -v docker &> /dev/null; then
+    echo "Installing Docker"
+    if ! sudo apt-get install apt-transport-https ca-certificates curl gnupg lsb-release -y; then
+        echo "Failed to install Docker dependencies, cleaning up..."
+        # Add your cleanup commands for Docker dependencies here.
+    elif ! curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg; then
+        echo "Failed to add Docker GPG key, cleaning up..."
+        # Add your cleanup commands for Docker GPG key here.
+    elif ! echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null; then
+        echo "Failed to add Docker repo, cleaning up..."
+        # Add your cleanup commands for Docker repo here.
+    elif ! sudo apt-get update -y; then
+        echo "Failed to update package lists, cleaning up..."
+        # Add your cleanup commands for package update here.
+    elif ! sudo apt-get install docker-ce docker-ce-cli containerd.io -y; then
+        echo "Failed to install Docker, cleaning up..."
+        # Add your cleanup commands for Docker installation here.
+    fi
+fi
 
-sudo apt-get update -y
-sudo apt-get install docker-ce docker-ce-cli containerd.io -y
-# sudo groupadd docker 
-# sudo usermod -aG docker $USER
-# newgrp docker 
-
-# install go
-echo "Installing go"
-sudo apt-get install wget
-wget https://golang.org/dl/go1.17.2.linux-amd64.tar.gz
-sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.17.2.linux-amd64.tar.gz
-echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.zshrc
-go version
-
-# install BT panel
-echo "Installing BT panel"
-wget -O install.sh http://download.bt.cn/install/install-ubuntu_6.0.sh && sudo bash install.sh -y
-
-# install anaconda.sh
-echo "Installing anaconda3"
-wget https://repo.anaconda.com/archive/Anaconda3-2021.05-Linux-x86_64.sh && sh ./Anaconda3-2021.05-Linux-x86_64.sh
+# Install Go if not present
+GO_VERSION="1.17.2"
+if ! go version | grep -q "$GO_VERSION"; then
+    echo "Installing Go $GO_VERSION"
+    if ! sudo apt-get install wget -y; then
+        echo "Failed to install wget, cleaning up..."
+        # Add your cleanup commands for wget here.
+    elif ! wget https://golang.org/dl/go${GO_VERSION}.linux-amd64.tar.gz; then
+        echo "Failed to download Go, cleaning up..."
+        # Add your cleanup commands for Go download here.
+    elif ! sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go${GO_VERSION}.linux-amd64.tar.gz; then
+        echo "Failed to install Go, cleaning up..."
+        # Add your cleanup commands for Go installation here.
+    elif ! echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.zshrc; then
+        echo "Failed to update PATH, cleaning up..."
+        # Add your cleanup commands for PATH update here.
+    elif ! go version; then
+        echo "Failed to check Go version, cleaning up..."
+        # Add your cleanup commands for Go version check here.
+    fi
+fi
